8149b47feb3d1a161792bfad1bd91902bad5d6e8
justDonnie <131680946+justDonnie@users.noreply.github.com>
2023-11-12 21:09:26 +0600

    Add files via upload

---
+++ LeetCodeSolution.java
@@ -0,0 +1,14 @@
+public class LeetCodeSolution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+        while (left <= right){
+            mid = (left + right) / 2;
+            if(nums[mid] == target) return mid;
+            else if (nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        return -1;
+    }
+}
