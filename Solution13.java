f40ebf16ed99ac00e6e2644fb8e1208a66aae8c8
justDonnie <131680946+justDonnie@users.noreply.github.com>
2023-12-04 18:02:38 +0300

    Add files via upload

---
+++ Solution13.java
@@ -0,0 +1,12 @@
+public class Solution13 {
+    public void moveZeroes(int[] nums) {
+        int i = -1, n = nums.length;
+        for (int j = 0; j < n; ++j) {
+            if (nums[j] != 0) {
+                int t = nums[++i];
+                nums[i] = nums[j];
+                nums[j] = t;
+            }
+        }
+    }
+}
